1bfaf2aa4d59b5702ec0c4dcbc834817bdf58de1
Soulahh <108687694+Soulahh@users.noreply.github.com>
2026-02-03 15:03:57 -0300

    ADD: Config-Manager class

--- src/config_manager.py
+++ src/config_manager.py
@@ -1,9 +1,17 @@
 import json
 
-def carregar_config():
-    with open("config.json", "r") as f:
-        dados = json.load()
+class ConfigManager():
+    def __init__(self):
+        self.json_path = "../config.json"
+    
+    def carregar_config(self):
+        try:
+            with open(self.json_path, "r") as f:
+                dados = json.load(f)
+                return dados
+        except FileNotFoundError as err:
+            return {}    
 
-    if "extensoes_permitidas" in dados:
-        dados["extensoes_permitidas"] = tuple(dados["extensoes_permitidas"])
-    return dados
+    def salvar_config(self, dados: dict):
+            with open(self.json_path, "w") as f:
+                json.dump(dados, f, indent=4)
