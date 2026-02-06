bf8a69afc75b457d95b0fdb5d9926fdf627cc9fc
Soulahh <108687694+Soulahh@users.noreply.github.com>
2026-02-06 00:40:10 -0300

    ADD: Funções de exibição de interface

--- src/cli_ui.py
+++ src/cli_ui.py
@@ -1,7 +1,40 @@
+logo = '''
+\t██████╗ ██████╗  ██████╗      ██╗███████╗ ██████╗████████╗    ██╗   ██╗███████╗██████╗ ██████╗  █████╗ ███╗   ██╗██████╗ ██╗
+\t██╔══██╗██╔══██╗██╔═══██╗     ██║██╔════╝██╔════╝╚══██╔══╝    ██║   ██║██╔════╝██╔══██╗██╔══██╗██╔══██╗████╗  ██║██╔══██╗██║
+\t██████╔╝██████╔╝██║   ██║     ██║█████╗  ██║        ██║       ██║   ██║█████╗  ██████╔╝██║  ██║███████║██╔██╗ ██║██║  ██║██║
+\t██╔═══╝ ██╔══██╗██║   ██║██   ██║██╔══╝  ██║        ██║       ╚██╗ ██╔╝██╔══╝  ██╔══██╗██║  ██║██╔══██║██║╚██╗██║██║  ██║██║
+\t██║     ██║  ██║╚██████╔╝╚█████╔╝███████╗╚██████╗   ██║        ╚████╔╝ ███████╗██║  ██║██████╔╝██║  ██║██║ ╚████║██████╔╝██║
+\t╚═╝     ╚═╝  ╚═╝ ╚═════╝  ╚════╝ ╚══════╝ ╚═════╝   ╚═╝         ╚═══╝  ╚══════╝╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝
+                                                                                                                            
+'''
+
+import socket
 import os
 import subprocess
 class CommandLineInterface():
     def __init__(self):
         self.clear_command = 'cls' if os.name == 'nt' else 'clear'
+        self.hostname = socket.gethostname()
     def display_logo(self):
         subprocess.call(self.clear_command, shell=True)
+        print(logo)
+    def iniciar_menu(self):
+        print(f"\tBem-vindo(a) ao Sistema Verdandi, {self.hostname}!")
+    def exibir_opcoes(self):
+        print(f"""
+        1 - Iniciar Monitoramento
+        2 - Alterar Configurações
+        3 - Encerrar Programa
+        """)
+    def exibir_configuracoes(self):
+        print(f"""
+            1 - Alterar Pasta de Origem
+            2 - Alterar Pasta de Destino
+            3 - Alterar Extensões Permitidas
+            4 - Alterar Exibição de Interface
+            5 - Alterar Delay
+        """)
+cli = CommandLineInterface()
+cli.display_logo()
+cli.iniciar_menu()
+cli.exibir_opcoes()
